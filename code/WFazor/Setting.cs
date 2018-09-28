5252b7a40fc8d83403a3d1e6f476425e4f1b4a15
Vo Son Ha <vosonha89@gmail.com>
2018-09-28 15:50:18 +0700

    Modify

--- code/WFazor/Setting.cs
+++ code/WFazor/Setting.cs
@@ -11,14 +11,14 @@ namespace WFazor
         public string ModelFolder { get; set; }
         public string ViewFolder { get; set; }
         public string ControllerFolder { get; set; }
-        public string RuntimeFolder { get; set; }
+        public string LayoutFolder { get; set; }
 
-        public Setting(string modelFolder = "Models", string viewFolder = "Views", string controllerFolder = "Controllers", string runtimeFolder = "Runtime")
+        public Setting(string modelFolder = "Models", string viewFolder = "Views", string controllerFolder = "Controllers", string runtimeFolder = "Layout")
         {
             ModelFolder = modelFolder;
             ViewFolder = viewFolder;
             ControllerFolder = controllerFolder;
-            RuntimeFolder = runtimeFolder;
+            LayoutFolder = runtimeFolder;
         }
     }
 }
--- code/WFazor/WFazorBrowser.cs
+++ code/WFazor/WFazorBrowser.cs
@@ -20,7 +20,7 @@ namespace WFazor
             string runTimePath = WFazorEngine.Instance.GetRuntimePath();
             string filePath = System.IO.Path.Combine(runTimePath, WFazorEngine.Instance.Setting.ViewFolder, controller, action + ".cshtml");
             string html = WFazorEngine.Instance.GetHtml(filePath);
-       
+
             this.DocumentText = html;
         }
     }
